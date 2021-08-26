2b94eb7835197abee7ef2377e51d617ed12676b0
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2021-08-26 14:57:58 +0200

    adjusted monitoring lxc to be a vm

--- docs_de/2._Services/5_monitoring_vm.md
+++ docs_de/2._Services/5_monitoring_vm.md
@@ -1,11 +1,18 @@
-# Monitoring (Debian 10.7)
+# Monitoring VM (Debian 10.7)
 
 ```shell
-apk add --update --no-cache curl
+apt-get update
+apt-get install -y curl sudo
+sudo -s
 
 export DOMAIN="grafana.secshell.net"
 export EMAIL="certificates@secshell.net"
 export CF_API_TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# DNS API Script
+export CHECK_DNS=1
+export UPDATE_DNS=1
+export CF_PROXIED='true'
 
 curl -fsSL https://docs.secshell.net/scripts/monitoring.sh | sh
 ```
--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -16,18 +16,16 @@ echo > /etc/motd
 # stop execution on failure
 set -e
 
-# enable hetzner apt repositories
-sed -i '1 i\deb http://mirror.hetzner.de/debian/packages buster main contrib non-free' /etc/apt/sources.list
-sed -i '2 i\deb http://mirror.hetzner.de/debian/security buster/updates main contrib non-free' /etc/apt/sources.list
-sed -i '3 i\deb http://mirror.hetzner.de/debian/packages buster-updates main contrib non-free' /etc/apt/sources.list
-
 # configure timezone
 ln -fs /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
+# comment out cdrom apt repositories
+sed -i -e '/deb cdrom.*/ s/^#*/#/' /etc/apt/sources.list
+
 apt-get update
 apt-get upgrade -y
-apt-get -y install sudo curl wget gnupg2 apt-transport-https software-properties-common git python3-pip
+apt-get -y install sudo curl wget gnupg2 apt-transport-https software-properties-common git python3-certbot-dns-cloudflare
 
 # grafana, influxdb, prometheus
 wget -qO- https://packages.grafana.com/gpg.key | gpg --dearmor > /etc/apt/trusted.gpg.d/grafana.gpg
@@ -42,19 +40,10 @@ apt-get -y install grafana influxdb prometheus
 # set capability to bind to port 443
 setcap 'cap_net_bind_service=+ep' /usr/sbin/grafana-server
 
-# install certbot
-python3 -m pip install certbot certbot-dns-cloudflare cryptography==3.2
-git clone https://github.com/cloudflare/python-cloudflare /tmp/python-cloudflare
-cd /tmp/python-cloudflare/
-python3 setup.py build
-python3 setup.py install
-cd
-rm -rf /tmp/python-cloudflare
-
 # get ssl certificate over dns-01 challenge
 echo "dns_cloudflare_api_token = ${CF_API_TOKEN}" > /root/.cloudflare.ini
 chmod 400 /root/.cloudflare.ini
-/usr/local/bin/certbot certonly \
+certbot certonly \
   --non-interactive \
   --agree-tos \
   --dns-cloudflare \
@@ -65,17 +54,24 @@ chmod 400 /root/.cloudflare.ini
 
 # enable https in grafana
 admin_password=$(cat /dev/urandom | tr -dc A-Za-z0-9 | fold -w 24 | head -n 1)
-echo "Grafana Credentials: admin / ${admin_password}"
 sed -i "s|;http_port.*|http_port = 443|g" /etc/grafana/grafana.ini
 sed -i "s|;protocol.*|protocol = https|g" /etc/grafana/grafana.ini
 sed -i "s|;cert_file.*|cert_file = /etc/letsencrypt/live/${DOMAIN}/cert.pem|g" /etc/grafana/grafana.ini
 sed -i "s|;cert_key.*|cert_key = /etc/letsencrypt/live/${DOMAIN}/privkey.pem|g" /etc/grafana/grafana.ini
 sed -i "s|;admin_password.*|admin_password = ${admin_password}|g" /etc/grafana/grafana.ini
 
+# grant grafana access to certificate files
 chown -R root:grafana /etc/letsencrypt
-chmod 755 /etc/letsencrypt/{live,archive}
+chmod -R 750 /etc/letsencrypt/{live,archive}
 
-systemctl daemon-reload
+# configure autostart
 systemctl enable --now grafana-server
 systemctl enable --now influxdb
 systemctl enable --now prometheus
+
+echo "Grafana Credentials: admin / ${admin_password}"
+
+# check dns
+if [ ${CHECK_DNS} -eq 1 ]; then
+  curl -fsSL https://docs.secshell.net/scripts/dns-api.sh | bash
+fi
